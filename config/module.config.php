8c94ffbe70a2231bebff4c7db9a8e74dd76b3b2f
Errol A. Zuñiga Jr <errol.zuniga@coredna.com>
2015-06-02 16:14:59 +0800

    Enhances module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	
+
 	'doctrine' => array(
 		'driver' => array(
 			'page_entities' => array(
@@ -8,7 +8,7 @@ return array(
 				'cache' => 'array',
 				'paths' => array(__DIR__ . '/../src/Page/Entity')
 			),
-			
+
 			'orm_default' => array(
 				'drivers' => array(
 					'Page\Entity' => 'page_entities'
@@ -24,25 +24,20 @@ return array(
 			'Admin\Page\Attachment' => 'Page\Controller\AttachmentController',
         ),
     ),
-	
+
 	'bjyauthorize' => array(
         'guards' => array(
-            'BjyAuthorize\Guard\Route' => array(
-				array('route' => 'admin/page', 'roles' => array('admin')),
-				array('route' => 'permalink', 'roles' => array('user','guest')),
-				array('route' => 'admin/attachment', 'roles' => array('admin')),
-            ),
 			'BjyAuthorize\Guard\Controller' => array(
-				array('controller' => 'Admin\Page',array(), 'roles' => array('admin')),
-				array('controller' => 'Admin\Page\Attachment',array(), 'roles' => array('admin')),
-				array('controller' => 'Page',array(), 'roles' => array('guest','user')),
+				array('controller' => 'Admin\Page', 'roles' => array('admin')),
+				array('controller' => 'Admin\Page\Attachment', 'roles' => array('admin')),
+				array('controller' => 'Page', 'roles' => array('guest','user')),
             ),
         ),
     ),
-	
+
 	'router' => array(
         'routes' => array(
-			
+
 			'permalink' => array(
 				'type'    => 'Segment',
 				'options' => array(
@@ -55,10 +50,10 @@ return array(
 						'permalink'     => '[a-zA-Z][a-zA-Z0-9_-]*',
 					),
 				),
-				
+
 				'may_terminate' => true,
 			),
-			
+
 			'page' => array(
 				'type'    => 'Segment',
 				'options' => array(
@@ -71,10 +66,10 @@ return array(
 						'id'     => '[0-9]*',
 					),
 				),
-				
+
 				'may_terminate' => true,
 			),
-			
+
 			'admin' => array(
                 'child_routes' => array(
                     'page' => array(
@@ -107,16 +102,16 @@ return array(
                     ),
                 ),
             ),
-			
+
         ),
     ),
-	
+
     'view_manager' => array(
         'template_path_stack' => array(
             'page' => __DIR__ . '/../view',
         ),
 		'template_map' => array(
-		
+
         ),
     ),
 );
